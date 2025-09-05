80181e65182d0200350c1212008b6cc533f3c377
tansanguy <2t6shg@gmail.com>
2025-09-05 19:40:43 +0900

    250905_1938_최종본

--- likes/serializers.py
+++ likes/serializers.py
@@ -5,7 +5,9 @@ from spaces.models import Space
 
 
 class LikeSerializer(serializers.ModelSerializer):
+    artist_id = serializers.IntegerField(source="artist.id", read_only=True)
     artist_name = serializers.CharField(source="artist.name", read_only=True)
+    space_id = serializers.IntegerField(source="space.id", read_only=True)
     space_name = serializers.CharField(source="space.place_name", read_only=True)
     liked = serializers.SerializerMethodField()
     user_info = serializers.SerializerMethodField()  # ✅ 좋아요 누른 사람 요약
@@ -14,21 +16,30 @@ class LikeSerializer(serializers.ModelSerializer):
         model = Like
         fields = [
             "id",
-            "user",
-            "user_info",     # ✅ user_id + nickname
-            "artist",
-            "space",
+            "artist_id",
             "artist_name",
+            "space_id",
             "space_name",
             "liked",
+            "user_info",
             "created_at",
         ]
-        read_only_fields = ["id", "created_at", "liked", "user_info"]
+        read_only_fields = [
+            "id",
+            "artist_id",
+            "artist_name",
+            "space_id",
+            "space_name",
+            "liked",
+            "user_info",
+            "created_at",
+        ]
 
     def get_liked(self, obj):
         return True
 
     def get_user_info(self, obj):
+        """유저 → role에 따라 닉네임/공간명 반환"""
         user_id = obj.user.id
         nickname = None
 
