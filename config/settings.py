83427151c086222af3a3d15ff2e6484307849389
sisihae <sihyunkim@snu.ac.kr>
2025-09-09 00:02:41 +0900

    environ settings

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+import environ
+
+# 환경변수 로더 초기화
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+# .env 파일 읽기 (로컬 개발 환경에서만)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k7041yzfj=n@kcws-p)k5s(ud(9&=e#tx15jjg28db*6ncs=em"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
