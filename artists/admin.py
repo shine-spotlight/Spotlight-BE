a79e1a6974e4be234d86d2e1af05f73cec750257
tansanguy <2t6shg@gmail.com>
2025-09-15 10:25:50 +0900

    isonboarding, role limit 수정, 아티스트 카태고리 배열로

--- artists/admin.py
+++ artists/admin.py
@@ -6,11 +6,14 @@ from artistequipments.models import ArtistEquipment   # ✅ 수정
 class ArtistAdmin(admin.ModelAdmin):
     list_display = (
         "id", "user", "name", "number_of_members",
-        "category", "custom_category",
+        "get_categories", "custom_category",
         "desired_pay", "is_free_allowed", "created_at",
     )
     search_fields = ("name", "user__kakao_id")
 
+    def get_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all()])
+    get_categories.short_description = "categories"
 
 @admin.register(ArtistEquipment)
 class ArtistEquipmentAdmin(admin.ModelAdmin):
