ecbc465921c45c58422754ab58495b340d8f7db0
tansanguy <2t6shg@gmail.com>
2025-09-17 20:25:46 +0900

    사진로직

--- config/settings.py
+++ config/settings.py
@@ -104,6 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
+import os
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Internationalization
